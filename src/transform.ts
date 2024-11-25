2f496b2c7928b88604744bb8f1d9238a877bfd5e
leon-js <42924541+leon-js@users.noreply.github.com>
2024-11-25 22:02:25 +0800

    feat(transform):添加className

--- src/transform.ts
+++ src/transform.ts
@@ -16,7 +16,7 @@ import type {packages} from "@babel/standalone";
 
 // const Babel = (window as any).Babel as { packages: typeof packages }// https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/7.26.2/babel.min.js
 
-export function transformRender(code: string, travelFn: ({tagName, attributeNames}) => Record<string, string>) {
+export function transformRender(code: string, travelFn: ({tagName, attributeNames}: {tagName: string, attributeNames: Set<string>}) => Record<string, string>) {
   //const { types, parser, traverse, generator } = Babel.packages;
 
   const ast = parser.parse(code, {
@@ -30,12 +30,16 @@ export function transformRender(code: string, travelFn: ({tagName, attributeName
   }
 
   /** 获取标签名 例如 Form.Item => Form */
-  const getTagName = (name: JSXIdentifier | JSXMemberExpression | JSXNamespacedName) => {
+  const getTagName = (name: JSXIdentifier | JSXMemberExpression | JSXNamespacedName, fullName: string = "") => {
     if (types.isJSXIdentifier(name)) {
-      return name.name
+      return {
+        name: name.name,
+        fullName: name.name + fullName
+      }
     } else if (types.isJSXMemberExpression(name)) {
-      return getTagName(name.object)
+      return getTagName(name.object, "-" + name.property.name + fullName)
     }
+    return { name: "", fullName }
   }
 
   /**
@@ -43,12 +47,16 @@ export function transformRender(code: string, travelFn: ({tagName, attributeName
    * import { Form, Button } from "antd"; => Form, Button
    */
   const importedDependencies = new Set();
+  const dependenciesToImported = new Map();
 
   traverse(ast, {
     ImportDeclaration(path) {
+      const dependency = path.node.source.value;
+
       path.get("specifiers").forEach((specifier) => {
         if (specifier.isImportSpecifier()) {
           importedDependencies.add(specifier.node.local.name);
+          dependenciesToImported.set(specifier.node.local.name, dependency);
         }
       });
     },
@@ -58,13 +66,38 @@ export function transformRender(code: string, travelFn: ({tagName, attributeName
       // }
 
       /** 标签名 */
-      const tagName = getTagName(path.node.name);
+      const { name: tagName, fullName } = getTagName(path.node.name);
 
       // 判断是否来自import
-      if (importedDependencies.has(tagName)) {
+      if (tagName && importedDependencies.has(tagName)) {
+        let classNameAttribute;
         const attributeNames = new Set(path.node.attributes.filter((attr) => {
           return types.isJSXAttribute(attr)
-        }).map((attr) => attr.name.name))
+        }).map((attr) => {
+          const attributeName = attr.name.name;
+
+          if (attributeName === "className") {
+            classNameAttribute = attr;
+            const { value } = attr;
+            if (types.isJSXExpressionContainer(value)) {
+              const { expression } = value;
+              if (types.isMemberExpression(expression)) {
+                value.expression = types.binaryExpression("+", expression, types.stringLiteral(`${dependenciesToImported.get(tagName)}-${fullName}`)) 
+              }
+            }
+          }
+
+          return attr.name.name as string
+        }))
+
+        if (!classNameAttribute) {
+          path.node.attributes.push(
+            types.jsxAttribute(
+              types.jsxIdentifier("className"),
+              types.stringLiteral(`${dependenciesToImported.get(tagName)}-${fullName}`),
+            ),
+          )
+        }
 
         // 扩展属性
         const extendJSXProps = travelFn({tagName, attributeNames})
